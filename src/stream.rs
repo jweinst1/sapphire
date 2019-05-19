fe139b86023a488ec21adbfe108f2927ae314322
Joshua Weinstein <jweinstein@splunk.com>
2019-05-19 14:50:38 -0700

    changing up the stream

--- src/stream.rs
+++ src/stream.rs
@@ -1,13 +1,33 @@
 use crate::parse::*;
 use crate::object::*;
 
+pub enum SaphCurrentBin {
+	First,
+	Second
+}
 
-struct SaphStream {
-	items:Vec<SaphObject>
+
+pub struct SaphStream {
+	bin_1:Vec<SaphObject>,
+	bin_2:Vec<SaphObject>,
+	state:SaphCurrentBin
 }
 
 impl SaphStream {
-	fn new(size:usize) -> Self {
-		SaphStream {items:Vec::with_capacity(size)}
+	pub fn new(size:usize) -> Self {
+		SaphStream {
+			bin_1:Vec::with_capacity(size),
+			bin_2:Vec::with_capacity(size),
+			state:SaphCurrentBin::First
+		}
+	}
+}
+
+impl SaphStream {
+	pub fn get_items(&self) -> &Vec<SaphObject> {
+		match self.state {
+			SaphCurrentBin::First => &self.bin_1,
+			SaphCurrentBin::Second => &self.bin_2
+		}
 	}
 }
